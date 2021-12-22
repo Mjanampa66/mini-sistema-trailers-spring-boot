624cb69974e3378221f8f2a8165ff0886704c4b3
PC\theuser <mjanampal66@gmail.com>
2021-12-22 17:54:11 -0500

    Agregando strategy al Id de Genero para el Id

--- mini-sistema-trailers/src/main/java/com/sistema/trailers/modelo/Genero.java
+++ mini-sistema-trailers/src/main/java/com/sistema/trailers/modelo/Genero.java
@@ -2,6 +2,8 @@ package com.sistema.trailers.modelo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -9,6 +11,7 @@ public class Genero {
 
 	@Id
 	@Column(name = "id_genero")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String titulo;
